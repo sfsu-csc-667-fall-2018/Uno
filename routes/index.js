a9938aaee33328588d7abf9ebb62f73b7d4ae24c
Caeg <chriscaeg@gmail.com>
2018-10-26 15:19:43 -0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-const express = require('express');
-let router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Knex = require('knex');
+const knex = Knex(require('../knexfile.js') [process.env.NODE_ENV || 'development'])
+
+
+router.get('/', function(req, res) {
+
+   res.render('index');
 });
 
+router.get('/register', function(req, res) {
+
+   knex('Users')
+   .insert({
+      UserName: req.query.username,
+      email: req.query.email,
+      Password: req.query.password
+   }).then(
+   res.redirect('/users'));
+});
+
+router.get('/users', function(req, res) {
+   knex('Users')
+   .then(
+      knex.select('UserName', 'email', 'Password').from('Users')
+      .then(function(users) {
+         //console.log(users.length);
+         res.render('users',{users: users});
+      }));
+});
+
+
 module.exports = router;
+
+   // req.query.username
