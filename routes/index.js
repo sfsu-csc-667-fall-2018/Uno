a48468ff936a6fab20d067a7367479b58a9072ae
preston <theteamvelociraptor@gmail.com>
2018-11-15 20:22:02 -0800

    skeleton of all current pages finished

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,28 @@ router.get('/login', function(req, res) {
     res.render('login');
 });
 
+router.get('/updatesuccess', function(req, res) {
+
+    res.render('updatesuccess');
+});
+
+
+router.get('/creategame', function(req, res) {
+
+    res.render('creategame');
+});
+
+router.get('/lobby', function(req, res) {
+
+    res.render('lobby');
+});
+
+
+router.get('/userinfo', function(req, res) {
+
+    res.render('userinfo');
+});
+
 router.get('/users', function(req, res) {
    knex('Users')
    .then(
