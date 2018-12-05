4d566d44b1ccc80f0e991cb276a99387fba2b756
pabloegpf1 <pabloescrivagallardo@gmail.com>
2018-12-04 21:58:56 -0800

    joined user to game

--- public/javascripts/lobby_actions.js
+++ public/javascripts/lobby_actions.js
@@ -2,18 +2,32 @@ const socket = io.connect();
 
 (() => {
 
-   $('#login-submit').on('click', event => {
-      console.log("clicked on login ===============");
+   $('#join-game').on('click', event => {
+      console.log("clicked on join game ===============");
       event.preventDefault();
       let user_info = {
-         'username':$('#login-username').val(),
-         'password':$('#login-password').val()}
-         socket.emit('join game',user_info);
-      });
+         'gameid':3
+      }
+      socket.emit('join game',user_info);
 
+      console.log("after clicked on join game ===============");
+   });
 
+   // function joinGame(){
+   //    console.log("clicked on join game ===============");
+   //    event.preventDefault();
+   //    let user_info = {
+   //       'gameid':3
+   //    }
+   //    socket.emit('join game',user_info);
+   // }
 
    socket.on('join game response', data => {
+      if(data.result == true){
+         console.log("user joined");
+      }else{
+         console.log("error joining user to game");
+      }
       //Preston fill in here for response from server
    });
 })();
--- socket/unoServer.js
+++ socket/unoServer.js
@@ -2,10 +2,11 @@ let users = {};
 let games = {};
 
 const unoServer = ( io, socket ,db ) => {
-  socket.join('uno');
-  socket.status = 'Online';
-  require('./users.js')(io, socket, db, users);
-  require('./listofgames.js')(io, socket, db, games,users);
+ socket.join('uno');
+ socket.status = 'Online';
+ require('./users.js')(io, socket, db, users);
+ require('./listofgames.js')(io, socket, db, games,users);
+ require('./user_events.js')(io, socket, db,users);
 };
 
 module.exports = unoServer;
