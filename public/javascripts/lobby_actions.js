4939ae29a80246620610bc6fb52ea5f731d024bf
notserpp <prestonsegura@yahoo.com>
2018-12-07 15:12:02 -0800

    update css in games

--- public/javascripts/lobby_actions.js
+++ public/javascripts/lobby_actions.js
@@ -37,26 +37,17 @@ const socket = io.connect();
       socket.emit('join game',user_info);
    }
 
-   //
-   // function updateGameList() {
-   //    let sample_games = [{gameid: 1, gamename: "poop", num_players:3}, {gameid: 2, gamename: "poop2", num_players:4}, {gameid: 3, gamename: "poop4", num_players:6}];
-   //    let searchelement = document.getElementsByClassName("current_games");
-   //    console.log(JSON.stringify(searchelement));
-   //
-   //    for(let i in sample_games) {
-   //        console.log('creating game ' + sample_games[i]["gamename"]);
-   //        let str = "<div id=game" + sample_games[i]["gameid"] + " class=\"col-md-4\">";//<a
-   //
-   //
-   //        str += "<div style=\"border: 2px solid black\"><p style=\"margin: 0; padding: 20px\"> Join game: " + sample_games[i]["gamename"] + "</p> <h4 style=\"margin: 0; padding: 20px\">players:" + sample_games[i]["num_players"] + "</h4></a></div>";
-   //
-   //    }
    function updateGameList(game_list) {
       for(let i in game_list) {
-         console.log('creatin game ' + game_list[i]["gamename"]);
-          let str = "<div id=game" + game_list[i]["gameid"] + " class=\"col-md-4\">";
+         console.log('creating game ' + game_list[i]["gamename"]);
+          let str = "<div id=game" + game_list[i]["gameid"] + " class=\"col-md-5\">";
 
-         str += "<div style=\"border: 2px solid black; margin: 20px\"><p style=\"padding: 2px\"> Join game: " + game_list[i]["gamename"] + "</p><h4>players:" + game_list[i]["num_players"] + "</h4></a></div>";
+         str += "<div style=\"border: 2px solid black; width: 100%; margin: 20px\">" +
+             "<center><h4>Game Name: </h4>" +
+             "<h1>" + game_list[i]["gamename"] + "</h1>" +
+             "<h4> Players: " + game_list[i]["num_players"] + "</h4>" +
+             "</a>" +
+             "</center></div>";
 
          let node = document.createElement('div');
          node.setAttribute("id",game_list[i]["gameid"]);
