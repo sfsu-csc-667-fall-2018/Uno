ff9342e736d0da092a6f5cf48399480074acbc0a
pabloegpf1 <pabloescrivagallardo@gmail.com>
2018-12-06 20:44:35 -0800

    migrated to heroku

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "use_env_variable":"DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{
+        "require":true
+      }
+    }
+  },
+  "test": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{
+        "require":true
+      }
+    }
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{
+        "require":true
+      }
+    }
+  }
+}
