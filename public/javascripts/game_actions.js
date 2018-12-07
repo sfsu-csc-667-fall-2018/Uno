48f4e3c4bb8b3e01af1a4443db4b0a170853bb32
moses_lee <moses_lee@apple.com>
2018-12-06 19:03:03 -0800

    Added stub for game actions

---
+++ public/javascripts/game_actions.js
@@ -0,0 +1,73 @@
+(() => {
+
+   $('#login-submit').on('click', event => {
+      console.log("clicked on login ===============");
+      event.preventDefault();
+      let user_info = {
+         'username':$('#login-username').val(),
+         'password':$('#login-password').val()}
+         socket.emit('login',user_info);
+      });
+
+   $('#register-submit').on('click', event => {
+      console.log("clicked on register ===============");
+      event.preventDefault();
+      let user_info = {
+         'username':$('#register-username').val(),
+         'email':$('#register-email').val(),
+         'password':$('#register-password').val(),}
+         socket.emit('register',user_info);
+      });
+
+
+    //Preston and Chris these are the calls to the server
+    //We need to wrap these in to functions
+    //I left some examples up above
+    // socket.emit('get num players', data);
+    // socket.emit('get player', data);
+    // socket.emit('get player data', data);
+    // socket.emit('get play result', data);
+    // socket.emit('current discard top card', data);
+    // socket.emit('get other player data', data);
+    // socket.emit('get current player points' data);
+    // socket.emit('get play', data);
+
+
+
+
+    //These functions are the call backs that the
+    //Server will call
+    socket.on('get num players response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get player response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get player data response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get play result response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('current discard top card response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get other player data response', data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get current player points response' data => {
+      //Preston and Chris fill in here
+    });
+
+    socket.on('get play response', data => {
+      //Preston and Chris fill in here
+    });
+
+
+})();
