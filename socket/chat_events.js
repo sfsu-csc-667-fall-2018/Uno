e8993cc7478b9f32754436961ddb7c4424d19b9e
pabloegpf1 <pabloescrivagallardo@gmail.com>
2018-12-14 11:49:04 -0800

    Cleaned game_Session file and moved queries to db directory

--- socket/chat_events.js
+++ socket/chat_events.js
@@ -3,9 +3,6 @@ const utilities = require('./utilities.js');
 const chat_events = (io, socket, users) => {
 
     let username = users[utilities.getUserId(socket)];
-
-    console.log("USERS MESSAGE"+JSON.stringify(username));
-
     socket.on('chat message', (message,users) => {
         console.log("NEW MESSAGE"+JSON.stringify(message));
         io.sockets.emit('chat message', {message : message.message, username : username.username});
--- socket/utilities.js
+++ socket/utilities.js
@@ -1,10 +1,8 @@
 
    function getUserId(socket){
-   console.log('================COOOKIES==================');
    let cookies = socket.handshake.headers['cookie'].split(';')
    for(let i = 0;i<cookies.length;i++){
       if(cookies[i].includes('connect.sid')){
-         console.log(cookies[i]);
          let start = cookies[i].indexOf('=');
          return cookies[i].slice(start+1);
       }
